0694c3d75067a5199937b1b0082531014c979b5d
Prateek Singh <prateeksingh9741@gmail.com>
2025-04-09 12:21:06 +0530

    create a card for AI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import AI from "@/components/ai/transcript-section";
 import { ModeToggle } from "@/components/theme-toggler";
 
 export default function Home() {
   return (
-    <div>
-      Hello world
-      <ModeToggle />
+    <div className="container h-full  mx-auto">
+      <div className="w-full flex justify-between my-2">
+        iThink
+        <ModeToggle />
+      </div>
+
+      <div className="grid h-full grid-rows-8 grid-cols-2">
+        <div className="row-start-4 row-end-6 col-span-full">
+          <div className="flex h-full gap-x-5">
+            <AI type="For" />
+            {/* <AI type="Against" /> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
